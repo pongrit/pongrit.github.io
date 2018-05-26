72fc2cc8d4a3353ea9f9d5ab82c7fdd3c6315c77
fullspectrum <psfreedom13@gmail.com>
2018-05-26 08:12:59 +0700

    0.27

--- duke_plugins.js
+++ duke_plugins.js
@@ -27,13 +27,20 @@ window.addEventListener("load", function(){
 /**
  * Facebook
 */
-(function(d, s, id) {
+/*(function(d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) return;
     js = d.createElement(s); js.id = id;
     js.src = "//connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.5";
     fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+}(document, 'script', 'facebook-jssdk'));*/
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/th_TH/sdk.js#xfbml=1&version=v3.0&appId=1657902257814402&autoLogAppEvents=1';
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
 
 
 
